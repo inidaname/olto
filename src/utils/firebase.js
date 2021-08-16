9c40476044743b7bd31fac3b2268c8326f3fe084
Hassan Sani <saniyhassan@gmail.com>
2021-08-16 02:10:33 +0100

    working on implementing tests

---
+++ src/utils/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBi9GbIi__X6gj6Ly0MjDVy5GDzFMgwEwk",
+  authDomain: "oltl-3471a.firebaseapp.com",
+  projectId: "oltl-3471a",
+  storageBucket: "oltl-3471a.appspot.com",
+  messagingSenderId: "929068208641",
+  appId: "1:929068208641:web:6c86c5772d1b1d15912600",
+  measurementId: "G-S8PH68T1MV"
+}
+
+export default firebaseConfig
