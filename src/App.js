54964d7af140ae514cecc07cbc0030ed842811e7
Hassan Sani <saniyhassan@gmail.com>
2021-08-18 00:39:14 +0100

    working on context

--- src/App.js
+++ src/App.js
@@ -2,10 +2,26 @@ import {Switch, Route, BrowserRouter as Router} from 'react-router-dom';
 import {Header} from 'components/';
 import { Login } from 'pages/';
 import './App.css';
+import { firebaseConfig } from 'util/firebase';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
-function App() {
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp(firebaseConfig)
+
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+const App = () => {
+
+  const [user] = useAuthState(auth);
   return (
     <div className="container">
+      {user ? 'save' : 'none'}
       <Header />
       <main>
         <Router>
