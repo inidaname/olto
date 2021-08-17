42f371730e3e9934ef61a8b8dfcc13d70f486d39
Hassan Sani <saniyhassan@gmail.com>
2021-08-17 16:42:15 +0100

    added firebase

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { FirebaseDatabaseProvider } from "@react-firebase/database";
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import './App.css';
-import firebaseConfig from './utils/firebase';
+import { Header } from 'components/';
+import { Login } from 'pages/';
+
 
 function App() {
   return (
-    <FirebaseDatabaseProvider firebase={firebaseConfig}>
-      <div className="App">
-        
-      </div>
-    </FirebaseDatabaseProvider>
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Login></Login>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {
+  ReactReduxFirebaseProvider,
+  firebaseReducer
+} from 'react-redux-firebase'
+import { combineReducers, createStore } from 'redux';
 import App from './App';
-import { store } from './app/store';
 import { Provider } from 'react-redux';
+import firebase from "firebase/app";
+import firebaseConfig from './utils/firebase';
 import * as serviceWorker from './serviceWorker';
+
+firebase.initializeApp(firebaseConfig)
+const rootReducer = combineReducers({
+  firebase: firebaseReducer
+})
+
+const rrfConfig = {
+  userProfile: 'users'
+  // useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
+}
+
+const initialState = {}
+const store = createStore(rootReducer, initialState)
+
+const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch
+  // createFirestoreInstance // <- needed if using firestore
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ReactReduxFirebaseProvider {...rrfProps}>
+        <App />
+      </ReactReduxFirebaseProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
