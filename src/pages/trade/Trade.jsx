1ced0c29887db7157aeadc8d2418a47bacf37f14
Hassan Sani <saniyhassan@gmail.com>
2021-08-28 03:56:45 +0100

    finished charts

--- src/pages/trade/Trade.jsx
+++ src/pages/trade/Trade.jsx
@@ -1,69 +1,118 @@
-import React, { memo, useContext, useEffect, useState } from "react";
-import { useFetch, useChart } from "hooks/";
+import React, { memo, useEffect, useState } from "react";
+import "./trade.css";
 import Chart from "react-apexcharts";
 
 const Trade = ({ data }) => {
-  const [chartData, setChartData] = useState([]);
+  const [monthData, setMonthData] = useState([]);
+  const [daysData, setDaysData] = useState([]);
   const [options, setOptions] = useState({});
-  const [months, setMonths] = useState([]);
-  const [average, setAverage] = useState([]);
 
   useEffect(() => {
     let dataseries = [];
-    let today = Date.now()
-    console.log(today)
+    let dataDaily = [];
+    let today = new Date();
+    const lastDays = new Date().setDate(today.getDate() - 3);
+    const monthsBack = new Date().setMonth(today.getMonth() - 3); // to get 3 months ago
+    let titleText = ''
     if (data)
       for (let i = 0; i < data.length; i++) {
         const trades = data[i];
+
         const open = trades.open?.toFixed(2);
         const high = trades.high?.toFixed(2);
-        const close = trades.close?.toFixed(2)
-        const low = trades.low?.toFixed(2)
-        dataseries.push({
-          x: new Date(trades.date * 1000),
-          y: [open, high, low, close],
-        });
+        const close = trades.close?.toFixed(2);
+        const low = trades.low?.toFixed(2);
+        if (monthsBack < trades.date * 1000) {
+          titleText = "Tesla (TSLA), last 3 months"
+          dataseries.push({
+            x: new Date(trades.date * 1000).toLocaleDateString("en-us", {
+              month: "short",
+              day: "2-digit",
+            }),
+            y: [open, high, low, close],
+          });
+        }
+
+        if (lastDays < trades.date * 1000) {
+          titleText = "Tesla (TSLA), last 3 days"
+          dataDaily.push({
+            x: new Date(trades.date * 1000).toLocaleDateString("en-us", {
+              month: "short",
+              day: "2-digit",
+            }),
+            y: [open, high, low, close],
+          });
+        }
       }
     setOptions({
       chart: {
         type: "candlestick",
         height: 350,
+        toolbar: false,
       },
       title: {
-        text: "CandleStick Chart",
-        align: "left",
+        text: titleText,
+        align: "center",
       },
       xaxis: {
         type: "datetime",
+        tickAmount: 6,
+        opposite: true,
+        labels: {
+          format: 'dd/MM',
+        },
       },
       yaxis: {
+        title: {
+          text: 'Price',
+        },
+        opposite: true,
         tooltip: {
           enabled: true,
         },
       },
+      legend: {
+        show: true,
+        position: 'right'
+      },
       style: {
-        colors: ['#F44336', '#E91E63', '#9C27B0']
-     }
+        colors: ["#F44336", "#E91E63", "#9C27B0"],
+      },
     });
 
-    setChartData([
+    setMonthData([
       {
         data: dataseries,
       },
     ]);
 
+    setDaysData([
+      {
+        data: dataDaily,
+      },
+    ]);
+
     return () => {};
   }, [data]);
+  console.log();
 
-  console.log(chartData.series);
   return (
-    <Chart
-      options={options}
-      series={chartData}
-      type="candlestick"
-      height={350}
-      width={675}
-    />
+    <div className="trades">
+      <Chart
+        options={options}
+        series={monthData}
+        type="candlestick"
+        height={350}
+        width={400}
+      />
+      <Chart
+        options={options}
+        series={daysData}
+        type="candlestick"
+        height={350}
+        width={400}
+      />
+    </div>
   );
 };
 
